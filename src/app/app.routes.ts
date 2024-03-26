4b8b0f6df38246e137a6bc252db86fe347aa6f6b
Bruno <121259934+amatbruno@users.noreply.github.com>
2024-03-26 23:31:26 +0100

    Homepage updates

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { ProductsComponent } from './products/products.component';
+import { FormProductsComponent } from './form-products/form-products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'products', component: ProductsComponent},
+    {path: 'add-products', component: FormProductsComponent},
+    {path: '', redirectTo: 'home', pathMatch: 'full'},
+];
---
+++ src/app/form-products/form-products.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-form-products',
+  standalone: true,
+  imports: [],
+  templateUrl: './form-products.component.html',
+  styleUrl: './form-products.component.css'
+})
+export class FormProductsComponent {
+
+}
