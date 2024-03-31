ea203b652d96c531a2c3e982fde60e4a21cafc9a
Bruno <121259934+amatbruno@users.noreply.github.com>
2024-03-31 17:31:54 +0200

    Form validations done

--- src/app/form-products/form-products.component.ts
+++ src/app/form-products/form-products.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form-products',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './form-products.component.html',
   styleUrl: './form-products.component.css'
 })
 export class FormProductsComponent {
 
+  productForm: FormGroup;
+
+  constructor() {
+    this.productForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+      price: new FormControl('', [Validators.required, Validators.max(750)]),
+      desc: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+    })
+  }
 }
