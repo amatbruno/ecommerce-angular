0ca85caf5880c727dacd8710daafb2d162e3d69e
Bruno <121259934+amatbruno@users.noreply.github.com>
2024-05-09 12:08:45 +0200

    Updates routes

--- src/app/constants/constants.ts
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyAuysvwxmkvSC9gyE6FkPAgxcKZxCorovU",
-    authDomain: "angular17-ecommerce-crud.firebaseapp.com",
-    databaseURL: "https://angular17-ecommerce-crud-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "angular17-ecommerce-crud",
-    storageBucket: "angular17-ecommerce-crud.appspot.com",
-    messagingSenderId: "16021722154",
-    appId: "1:16021722154:web:c8b9bcc3f04274cd8cdf53"
-};
