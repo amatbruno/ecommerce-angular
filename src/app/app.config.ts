9f77b6006c3b347524487143083c915b28843846
Bruno <121259934+amatbruno@users.noreply.github.com>
2024-05-09 12:07:52 +0200

    Updates routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,8 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { firebaseConfig } from './constants/constants';
+import { environment } from '../environments/environment';
+const firebaseConfig = environment.firebaseConfig;
 
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
