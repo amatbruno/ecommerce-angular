1233ca96136a64666376cc95685764b196ad4e4e
Bruno <brunoamagar@campus.monlau.com>
2024-05-13 18:29:05 +0200

    updates

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,23 +2,17 @@ import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { environment } from '../environments/environment';
-const firebaseConfig = environment.firebaseConfig;
 
-import { AngularFireModule } from "@angular/fire/compat";
-import { AngularFireAuthModule } from "@angular/fire/compat/auth";
-import { AngularFireDatabaseModule } from "@angular/fire/compat/database";
-import { AngularFirestoreModule } from "@angular/fire/compat/firestore";
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom([
-      AngularFireModule.initializeApp(firebaseConfig),
-      AngularFireAuthModule,
-      AngularFireDatabaseModule,
-      AngularFirestoreModule
+      HttpClientModule,
+      FormsModule
     ]),
   ]
 };
